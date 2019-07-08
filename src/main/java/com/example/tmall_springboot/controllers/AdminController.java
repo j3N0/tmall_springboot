212d579fb4e58cabebf0c17da0d17e65b31f7803
j3N0 <3223529700@qq.com>
2019-07-08 15:23:15 +0800

    fix typo. #2

--- src/main/java/com/example/tmall_springboot/controllers/AdminController.java
+++ src/main/java/com/example/tmall_springboot/controllers/AdminController.java
@@ -1,9 +1,9 @@
 package com.example.tmall_springboot.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class AdminController {
 
     @GetMapping("/admin")
