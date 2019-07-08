b16c39d47541475394f6b0c9bf425e65196efd75
j3N0 <3223529700@qq.com>
2019-07-08 14:07:59 +0800

    added admin page controller. #2

---
+++ src/main/java/com/example/tmall_springboot/controllers/AdminController.java
@@ -0,0 +1,63 @@
+package com.example.tmall_springboot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "redirect:admin_category_list";
+    }
+
+    @GetMapping("/admin_category_list")
+    public String listCategories() {
+        return "admin/listCategory";
+    }
+
+    @GetMapping("/admin_category_edit")
+    public String editCategory() {
+        return "admin/editCategory";
+    }
+
+    @GetMapping(value="/admin_order_list")
+    public String listOrder() {
+        return "admin/listOrder";
+    }
+
+    @GetMapping(value="/admin_product_list")
+    public String listProduct() {
+        return "admin/listProduct";
+    }
+
+    @GetMapping(value="/admin_product_edit")
+    public String editProduct() {
+        return "admin/editProduct";
+    }
+    @GetMapping(value="/admin_productImage_list")
+    public String listProductImage() {
+        return "admin/listProductImage";
+    }
+
+    @GetMapping(value="/admin_property_list")
+    public String listProperty() {
+        return "admin/listProperty";
+    }
+
+    @GetMapping(value="/admin_property_edit")
+    public String editProperty() {
+        return "admin/editProperty";
+    }
+
+    @GetMapping(value="/admin_propertyValue_edit")
+    public String editPropertyValue() {
+        return "admin/editPropertyValue";
+    }
+
+    @GetMapping(value="/admin_user_list")
+    public String listUser() {
+        return "admin/listUser";
+    }
+
+}
