fb88878c7f2027adf3d7631d5ad36a56ab9dbc09
j3N0 <3223529700@qq.com>
2019-07-08 14:15:39 +0800

    Implemented Page Wrapper class for navigator. #2

---
+++ src/main/java/com/example/tmall_springboot/utils/Page4Navigator.java
@@ -0,0 +1,111 @@
+package com.example.tmall_springboot.utils;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Page4Navigator<T> {
+
+    Page<T> pageFromJPA;
+
+    int navigatePages;
+
+    int totalPages;
+
+    int number;
+
+    long totalElements;
+
+    int size;
+
+    int numberOfElements;
+
+    List<T> content;
+
+    boolean isHasContent;
+
+    boolean first;
+
+    boolean last;
+
+    boolean isHasNext;
+
+    boolean isHasPrevious;
+
+    int[] navigatePageNums;
+
+    public Page4Navigator(Page<T> pageFromJPA, int navigatePages) {
+
+        this.pageFromJPA = pageFromJPA;
+
+        this.navigatePages = navigatePages;
+
+        totalPages = pageFromJPA.getTotalPages();
+
+        number  = pageFromJPA.getNumber() ;
+
+        totalElements = pageFromJPA.getTotalElements();
+
+        size = pageFromJPA.getSize();
+
+        numberOfElements = pageFromJPA.getNumberOfElements();
+
+        content = pageFromJPA.getContent();
+
+        isHasContent = pageFromJPA.hasContent();
+
+        first = pageFromJPA.isFirst();
+
+        last = pageFromJPA.isLast();
+
+        isHasNext = pageFromJPA.hasNext();
+
+        isHasPrevious  = pageFromJPA.hasPrevious();
+
+        calcNavigatepageNums();
+
+    }
+
+    private void calcNavigatepageNums() {
+        int navigatePageNums[];
+        int totalPages = getTotalPages();
+        int num = getNumber();
+        //当总页数小于或等于导航页码数时
+        if (totalPages <= navigatePages) {
+            navigatePageNums = new int[totalPages];
+            for (int i = 0; i < totalPages; i++) {
+                navigatePageNums[i] = i + 1;
+            }
+        } else { //当总页数大于导航页码数时
+            navigatePageNums = new int[navigatePages];
+            int startNum = num - navigatePages / 2;
+            int endNum = num + navigatePages / 2;
+
+            if (startNum < 1) {
+                startNum = 1;
+                //(最前navigatePages页
+                for (int i = 0; i < navigatePages; i++) {
+                    navigatePageNums[i] = startNum++;
+                }
+            } else if (endNum > totalPages) {
+                endNum = totalPages;
+                //最后navigatePages页
+                for (int i = navigatePages - 1; i >= 0; i--) {
+                    navigatePageNums[i] = endNum--;
+                }
+            } else {
+                //所有中间页
+                for (int i = 0; i < navigatePages; i++) {
+                    navigatePageNums[i] = startNum++;
+                }
+            }
+        }
+        this.navigatePageNums = navigatePageNums;
+    }
+}
