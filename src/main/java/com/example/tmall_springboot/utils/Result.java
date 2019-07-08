5a22e874cb3826d532daff3310b923e51fd23539
j3N0 <3223529700@qq.com>
2019-07-08 15:37:32 +0800

    Implemented Result class. #2

---
+++ src/main/java/com/example/tmall_springboot/utils/Result.java
@@ -0,0 +1,32 @@
+package com.example.tmall_springboot.utils;
+
+import lombok.Data;
+
+@Data
+public class Result {
+
+    public static int SUCCESS_CODE = 0;
+    public static int FAIL_CODE = 1;
+
+    int code;
+    String message;
+    Object data;
+
+    private Result(int code, String message, Object data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static Result success() {
+        return new Result(SUCCESS_CODE,null,null);
+    }
+
+    public static Result success(Object data) {
+        return new Result(SUCCESS_CODE,"",data);
+    }
+
+    public static Result fail(String message) {
+        return new Result(FAIL_CODE,message,null);
+    }
+}
