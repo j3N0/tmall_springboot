6d06c99395726b2240a988b558453c9e3a1caf96
j3N0 <3223529700@qq.com>
2019-07-08 15:27:41 +0800

    added getStatusDesc method. #2

--- src/main/java/com/example/tmall_springboot/domains/Order.java
+++ src/main/java/com/example/tmall_springboot/domains/Order.java
@@ -1,13 +1,12 @@
 package com.example.tmall_springboot.domains;
 
+import com.example.tmall_springboot.services.OrderService;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
-
-//import com.how2j.tmall_springboot.services.OrderService;
 
 @Entity
 @Data
@@ -44,34 +43,35 @@ public class Order {
     @Transient
     private String statusDesc;
 
-//    public String getStatusDesc(){
-//        if(null!=statusDesc)
-//            return statusDesc;
-//        String desc = "未知";
-//        switch(status) {
-//            case OrderService.waitPay:
-//                desc="待付";
-//                break;
-//            case OrderService.waitDelivery:
-//                desc="待发";
-//                break;
-//            case OrderService.waitConfirm:
-//                desc="待收";
-//                break;
-//            case OrderService.waitReview:
-//                desc="等评";
-//                break;
-//            case OrderService.finish:
-//                desc="完成";
-//                break;
-//            case OrderService.delete:
-//                desc="刪除";
-//                break;
-//            default:
-//                desc="未知";
-//        }
-//        statusDesc = desc;
-//        return statusDesc;
-//    }
+    //TODO implement DTO
+    public String getStatusDesc(){
+        if(null!=statusDesc)
+            return statusDesc;
+        String desc = "未知";
+        switch(status) {
+            case OrderService.waitPay:
+                desc="待付";
+                break;
+            case OrderService.waitDelivery:
+                desc="待发";
+                break;
+            case OrderService.waitConfirm:
+                desc="待收";
+                break;
+            case OrderService.waitReview:
+                desc="等评";
+                break;
+            case OrderService.finish:
+                desc="完成";
+                break;
+            case OrderService.delete:
+                desc="刪除";
+                break;
+            default:
+                desc="未知";
+        }
+        statusDesc = desc;
+        return statusDesc;
+    }
 
 }
