84dbe5cffe8031c547630e6aff41683331c689d5
j3N0 <3223529700@qq.com>
2019-07-08 13:20:32 +0800

    init sprinboot project.

---
+++ src/main/java/com/example/tmall_springboot/TmallSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.tmall_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmallSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmallSpringbootApplication.class, args);
+    }
+
+}
