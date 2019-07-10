a9f12826db5cc8ff27cd77457e45aa43ab9e86a7
j3N0 <3223529700@qq.com>
2019-07-10 13:14:39 +0800

    added UserService methods. #5

--- src/main/java/com/example/tmall_springboot/services/UserService.java
+++ src/main/java/com/example/tmall_springboot/services/UserService.java
@@ -3,4 +3,10 @@ package com.example.tmall_springboot.services;
 import com.example.tmall_springboot.domains.User;
 
 public interface UserService extends PageService<User> {
+
+    boolean isExist(String name);
+
+    User getByName(String name);
+
+    void add(User user);
 }
