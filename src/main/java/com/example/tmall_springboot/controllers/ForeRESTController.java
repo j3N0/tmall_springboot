72ef02fee66e23708d69066e15c8f28dab6589bb
j3N0 <3223529700@qq.com>
2019-07-10 13:24:50 +0800

    Implemented ForeRESTController register api. #5

--- src/main/java/com/example/tmall_springboot/controllers/ForeRESTController.java
+++ src/main/java/com/example/tmall_springboot/controllers/ForeRESTController.java
@@ -1,10 +1,16 @@
 package com.example.tmall_springboot.controllers;
 
 import com.example.tmall_springboot.domains.Category;
+import com.example.tmall_springboot.domains.User;
 import com.example.tmall_springboot.services.CategoryService;
 import com.example.tmall_springboot.services.ProductService;
+import com.example.tmall_springboot.services.UserService;
+import com.example.tmall_springboot.utils.Result;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -13,10 +19,12 @@ public class ForeRESTController {
 
     private final CategoryService categoryService;
     private final ProductService productService;
+    private final UserService userService;
 
-    public ForeRESTController(CategoryService categoryService, ProductService productService) {
+    public ForeRESTController(CategoryService categoryService, ProductService productService, UserService userService) {
         this.categoryService = categoryService;
         this.productService = productService;
+        this.userService = userService;
     }
 
     @GetMapping("/forehome")
@@ -27,4 +35,25 @@ public class ForeRESTController {
         categoryService.removeCategoryFromProduct(categoryList);
         return categoryList;
     }
+
+    @PostMapping("/foreregister")
+    public Object register(@RequestBody User user) {
+        String name =  user.getName();
+        String password = user.getPassword();
+        name = HtmlUtils.htmlEscape(name);
+        user.setName(name);
+        boolean exist = userService.isExist(name);          //TODO can implement Optional
+
+        if(exist){
+            String message ="用户名已经被使用,不能使用";
+            return Result.fail(message);
+        }
+
+        user.setPassword(password);
+
+        userService.add(user);
+
+        return Result.success();
+    }
+
 }
